0865380f01a57cbb0b09418a86db462dcddda4b8
joshuahenry3108 <joshua.sahayam@rootquotient.com>
2026-01-09 18:45:48 +0530

    Change server port from 9000 to 8080

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
